ec6ecd42c37ec0213b2e628dc58b7c7141ffaa51
Marcelo Garcia <marcelomgarcia@gmail.com>
2023-06-21 22:45:18 +0300

    Read of sample file

--- populate.py
+++ populate.py
@@ -4,12 +4,31 @@
 
 import sqlite3
 
+def read_sample_file(sample_file):
+    """
+    Read a file with sample items to test insertion into the db.
+    """
+
+    with open(sample_file, 'r') as fin:
+        text = fin.readlines()
+
+    text = [line.strip() for line in text]
+
+    return text
+
 def main():
     sample_file = 'sample_items.txt'
 
     # Define some date in the past to test ingesting the item again
     # in a date in the future.
     ingest_date = "2023-06-10"
+
+    # Read sample items file
+    ingested_list = read_sample_file(sample_file)
+
+    print(ingested_list)
+
+    
 
 if __name__ == "__main__":
     main()
