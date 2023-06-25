c4f5283b5a1e59f395cdb81be439459d9c33bd14
Marcelo Garcia <marcelomgarcia@gmail.com>
2023-06-25 12:01:29 -0300

    formatted document

--- populate.py
+++ populate.py
@@ -1,13 +1,14 @@
-# 
+#
 # Populate (ingest) a few items to simulate the ingestion process.
 #
 
 import sqlite3
 
-def insert_db(items:list, date:str) -> None:
+
+def insert_db(items: list, date: str) -> None:
     """Insert 'items' in the DB."""
 
-    conn = sqlite3.connect('r2p.db')
+    conn = sqlite3.connect("r2p.db")
     cur = conn.cursor()
 
     for item in items:
@@ -18,20 +19,22 @@ def insert_db(items:list, date:str) -> None:
     conn.commit()
     print(f"Total changes: '{conn.total_changes}'")
 
+
 def read_sample_file(sample_file):
     """
     Read a file with sample items to test insertion into the db.
     """
 
-    with open(sample_file, 'r') as fin:
+    with open(sample_file, "r") as fin:
         text = fin.readlines()
 
     text = [line.strip() for line in text]
 
     return text
 
+
 def main():
-    sample_file = 'sample_items.txt'
+    sample_file = "sample_items.txt"
 
     # Define some date in the past to test ingesting the item again
     # in a date in the future.
@@ -43,7 +46,7 @@ def main():
     # print(ingested_list)
 
     insert_db(ingested_list, ingest_date)
-    
+
 
 if __name__ == "__main__":
     main()
